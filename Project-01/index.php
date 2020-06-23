dafdc63f771e602dd624f8a410d8a638dd894dcb
Bruno Alves <brunoalveslp200597@gmail.com>
2020-06-22 22:25:14 -0300

    stylesheet and responsivity

--- Project-01/index.php
+++ Project-01/index.php
@@ -7,82 +7,87 @@
     <meta name="description" content="Php project to practice php, html5, css3, mySQL,and more.">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/font-awesome.min.css">
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:300,400,700" rel="stylesheet">
     <title>Project-01</title>
 </head>
 <body>
     <header>
-        
-            <div class="logo">Logomarca</div>
-            <div class="desktop">
+        <div class="logo">Logomarca</div>
+            <nav class="desktop">
+                
                 <ul>
                     <li><a href="">Home</a></li>
                     <li><a href="">Sobre</a></li>
                     <li><a href="">Serviços</a></li>
                     <li><a href="">Contato</a></li>
                 </ul>
-            </div><!--header desltop-->
+            </nav><!--header desltop-->
         
-            <div class="mobile">
+            <nav class="mobile">
+                <div class="mobile-menu"><i class="fa fa-bars" aria-hidden="true"></i></div>
                 <ul>
                     <li><a href="">Home</a></li>
                     <li><a href="">Sobre</a></li>
                     <li><a href="">Serviços</a></li>
                     <li><a href="">Contato</a></li>
                 </ul>
-            </div><!--header mobile-->
+            </nav><!--header mobile-->
         
     </header>
     
-    <section class="main-Banner">
+    <section class="main-banner">
+        <div class="overlay"></div>
         <div class="center">
         <form action="">
             <h2>Qual o seu melhor E-mail?</h2>
             <input type="email" name="email" required>
-            <input type="submit" name="action" value="cadastrar">
+            <input type="submit" name="action" value="Cadastrar!">
         </form>
         </div><!--center-->
     </section><!--main banner-->
 
     <section class="author-description">
         <div class="center author">
-        <div class="w50">
-                <img src="./images/BrunoAlves.jpg" alt="author photografy">
-            </div>
-            <div class="w50">
+            <div class="w50 left">
                 <h2>Bruno Alves</h2>
         
-                <p>Curabitur viverra nec lorem a hendrerit. Quisque euismod sem eu interdum congue. Cras ex ante, lacinia vel risus scelerisque, sodales ultrices diam. Vivamus sit amet mauris eu mi facilisis congue nec nec magna. Nulla facilisi. Aenean in sapien lorem. Nam sollicitudin ac augue id varius. Duis venenatis ultricies pharetra. Phasellus ut nibh tortor. Curabitur ultricies nibh in erat lacinia efficitur id tincidunt odio. Nunc venenatis sollicitudin lorem, eget vulputate sapien hendrerit sed. Praesent nec nunc ultricies quam aliquam tincidunt eget vel dui. Nam est libero, posuere sed leo ut, pellentesque scelerisque ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.</p>
-                
+                <p>Curabitur viverra nec lorem a hendrerit. Quisque euismod sem eu interdum congue. Cras ex ante, lacinia vel risus scelerisque, sodales ultrices diam. Vivamus sit amet mauris eu mi facilisis congue nec nec magna. Nulla facilisi. Aenean in sapien lorem. Nam sollicitudin ac augue id varius. Duis venenatis ultricies pharetra. Phasellus ut nibh tortor. Curabitur ultricies nibh in erat lacinia efficitur id tincidunt odio. Nunc venenatis sollicitudin lorem, eget vulputate sapien hendrerit sed. Praesent nec nunc ultricies quam aliquam tincidunt eget vel dui. Nam est libero, posuere sed leo ut, pellentesque scelerisque ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.
+                </p>
+                <p>Curabitur viverra nec lorem a hendrerit. Quisque euismod sem eu interdum congue. Cras ex ante, lacinia vel risus scelerisque, sodales ultrices diam. Vivamus sit amet mauris eu mi facilisis congue nec nec magna. Nulla facilisi. Aenean in sapien lorem. Nam sollicitudin ac augue id varius. Duis venenatis ultricies pharetra. Phasellus ut nibh tortor. Curabitur ultricies nibh in erat lacinia efficitur id tincidunt odio. Nunc venenatis sollicitudin lorem, eget vulputate sapien hendrerit sed. Praesent nec nunc ultricies quam aliquam tincidunt eget vel dui. Nam est libero, posuere sed leo ut, pellentesque scelerisque ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.
+                </p>
             </div>
-
+            <div class="w50 right">
+                <img class="right" src="./images/BrunoAlves.jpg" alt="author photografy">
+            </div>
             
         </div><!--center-->
 
     </section><!--author description-->
     <section class="specialities">
-        
-
+    
         <div class="center">
             <h2 class="title">Especialidades</h2>
-
             <div class="spec">
-            <div class="box-spec">
-                <i class="fa fa-html5" aria-hidden="true"></i>
+               
+            <div class="box-spec w33">
+                <div class="icon"><i class="fa fa-html5" aria-hidden="true"></i></div>
+                
                 <h3>HTML5</h3>
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id tortor at eros fringilla tempus vel eget justo. Ut auctor lectus ut justo pharetra bibendum. Etiam ligula sem, iaculis nec bibendum sit amet, scelerisque nec mi. Fusce in ultrices felis, eu aliquet velit. Sed dictum mollis ipsum in dignissim. Sed scelerisque, quam non dictum aliquam.</p>
             </div>   
 
-            <div class="box-spec">
-            <i class="fa fa-css3" aria-hidden="true"></i>
+            <div class="box-spec w33">
+                <div class="icon"><i class="fa fa-css3" aria-hidden="true"></i></div>
+            
                 <h3>CSS3</h3>
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id tortor at eros fringilla tempus vel eget justo. Ut auctor lectus ut justo pharetra bibendum. Etiam ligula sem, iaculis nec bibendum sit amet, scelerisque nec mi. Fusce in ultrices felis, eu aliquet velit. Sed dictum mollis ipsum in dignissim. Sed scelerisque, quam non dictum aliquam.</p>
             </div>  
 
-            <div class="box-spec">
-            <i class="fa fa-scribd" aria-hidden="true"></i>
+            <div class="box-spec w33">
+                <div class="icon"><i class="fa fa-superpowers" aria-hidden="true"></i></div>
+            
                 <h3>JavaScript</h3>
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id tortor at eros fringilla tempus vel eget justo. Ut auctor lectus ut justo pharetra bibendum. Etiam ligula sem, iaculis nec bibendum sit amet, scelerisque nec mi. Fusce in ultrices felis, eu aliquet velit. Sed dictum mollis ipsum in dignissim. Sed scelerisque, quam non dictum aliquam.</p>
